4ea539bd04466f91cb1e142f58f5e381805ac186
sarveshwar22 <f20190257@goa.bits-pilani.ac.in>
2020-07-26 02:06:38 +0530

    nose detected

--- movement.py
+++ movement.py
@@ -9,4 +9,15 @@ def detect_nose(img, faceCascade):
     gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
     #features is detected in gray-scale image which returns coordinates,width and height of features
-    
+    features = faceCascade.detectMultiScale(gray_img,1.1,8)
+    nose_cords = []
+
+    #drawing rectangle around feature
+    for(x,y,w,h) in features:
+        cv2.circle(img,(((2*x+w)//2),(2*y+h)//2),10,color['green'],2)
+        #to see face-boundary too
+        #cv2.rectangle(img,(x,y),(x+w,y+h),color['green'],2)
+        nose_cords = ((2*x+w)//2,(2*y+h)//2)
+
+    return img, nose_cords
+
